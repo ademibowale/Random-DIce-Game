083fe292deb9509bcfcdcda8b8ee7fd2b4399b18
ademibowale <92458236+ademibowale@users.noreply.github.com>
2022-03-13 16:10:24 +0100

    commit change

--- index.js
+++ index.js
@@ -9,24 +9,25 @@ function editNames() {
     document.querySelector("p.Player1").innerHTML = player1;
     document.querySelector("p.Player2").innerHTML = player2;
 }
-/*
+
 // Function to roll the dice
 function rollTheDice() {
-    setTimeout(function() {
-                var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-                var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    //function setTimeout() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-                document.querySelector(".img1").setAttribute("src",
-                    "dice" + randomNumber1 + ".png");
+    document.querySelector(".img1").setAttribute("src",
+        "dice" + randomNumber1 + ".png");
 
-                document.querySelector(".img2").setAttribute("src",
-                    "dice" + randomNumber2 + ".png");
+    document.querySelector(".img2").setAttribute("src",
+        "dice" + randomNumber2 + ".png");
 
-                if (randomNumber1 === randomNumber2) {
-                    document.querySelector("h1").innerHTML = "Draw!";
-                } else if (randomNumber1 < randomNumber2) {
-                    document.querySelector("h1").innerHTML = (player2 + " WINS!");
-                } else {
-                    document.querySelector("h1").innerHTML = (player1 + " WINS!");
-                }
-            }*/
+    if (randomNumber1 === randomNumber2) {
+        document.querySelector("h1").innerHTML = "Draw!";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = (player2 + " WINS!");
+    } else {
+        document.querySelector("h1").innerHTML = (player1 + " WINS!");
+    }
+}
+//}
